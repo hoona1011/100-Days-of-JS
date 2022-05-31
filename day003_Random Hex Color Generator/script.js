2323bee04292e01856cb350218f814e43c0598b7
hoona1011 <jasonpark90@naver.com>
2022-05-31 10:59:23 +0900

    Pushing for cafe

--- day003_Random Hex Color Generator/script.js
+++ day003_Random Hex Color Generator/script.js
@@ -1,17 +1,20 @@
-const hex = document.querySelector(".hex");
+const hex = document.querySelector(".color");
 const btn = document.querySelector(".generate");
 const container = document.querySelector(".counter-div");
 
 const generateColor = () => {
   const randomColor = Math.random().toString(16).substring(2, 8);
   container.style.backgroundColor = "#" + randomColor;
+
   hex.innerHTML = "#" + randomColor;
-  console.log("제너레이터 동작!");
+  console.log("함수호출");
+
+  setTimeout(() => {
+    generateColor();
+  }, 200);
 };
 
 btn.addEventListener("click", generateColor);
-
-generateColor();
 
 // let color = Math.random();
 // color = Math.random().toString(16);
