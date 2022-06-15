ab6ba71cd201f6cccaf3ce79b4071018c7a8dd04
hoona1011 <jasonpark90@naver.com>
2022-06-15 15:44:02 +0900

    refactor: Use the way that reference array elements randomly instead of shuffle function

---
+++ day014-Ajax-Quotes-Generator/fetch.js
@@ -0,0 +1,62 @@
+const btn = document.querySelector(".get-quotes");
+const number = document.getElementById("number");
+const URL = "https://type.fit/api/quotes";
+const quotesEl = document.querySelector(".quotes");
+
+btn.addEventListener("click", getQuotes);
+
+function getQuotes(e) {
+  e.preventDefault();
+
+  if (number.value.length == 0) {
+    return alert("숫자를 입력해주세요");
+  } else {
+    fetch(URL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        //셔플은 필요 이상의 연산을 하는 단점이 있음
+        // shuffle(data);
+
+        let output = "";
+
+        for (let i = 0; i < data.length; i += 1) {
+          const randomNumber = Math.floor(Math.random() * data.length);
+          if (i == number.value) {
+            break;
+          }
+          output += `
+                    <li>명언: ${data[randomNumber].text}</li>
+                    <li>인물: ${data[randomNumber].author}</li>
+                   <hr>
+               `;
+        }
+        quotesEl.innerHTML = output;
+      });
+  }
+}
+
+//  FUNCTION TO SHUFFLE QUOTES
+
+function shuffle(quotes) {
+  let CI = quotes.length;
+  let tempValue;
+  let randomIndex;
+
+  // While elements exist in the array
+  while (CI > 0) {
+    randomIndex = Math.floor(Math.random() * CI);
+
+    console.log("한 번 섞었음");
+
+    // CI 1 감소
+    CI--;
+    // SWAP THE LAST ELEMENT WITH CI
+    tempValue = quotes[CI]; // 임시로 tempValue에 quotes[CI]값을 저장해놓고
+    quotes[CI] = quotes[randomIndex]; // randomIndex 인덱스에 해당하는 요소의 값을 quotes[CI]에 할당
+    quotes[randomIndex] = tempValue;
+    // 마치 저글링처럼 tempValue를 중간 저장소로 이용해서 quotes[CI]와 quotes[randomIndex]의 값을 교환해주는 역할
+  }
+  return quotes;
+}
